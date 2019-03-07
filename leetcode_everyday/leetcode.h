0e8d3053156bc4e623982afac3e19357614b962d
luxiaohua <luxiaohua@outlook.com>
2019-03-07 13:36:50 +0800

    permute

--- leetcode_everyday/leetcode.h
+++ leetcode_everyday/leetcode.h
@@ -10,10 +10,10 @@
 *		2.3.如果遇到一个结点，左孩子不为空，右孩子为空；或者左右孩子都为空；
 *			则该节点之后的队列中的结点都为叶子节点；
 */
-bool isCompleteTree(TreeNode* root) 
+bool isCompleteTree(TreeNode* root)
 {
 	if (root == nullptr) return false;
-	
+
 	queue<TreeNode*> tq;
 	tq.push(root);
 
@@ -45,49 +45,9 @@ bool isCompleteTree(TreeNode* root)
 }
 
 
-
-//int sumNumbers(TreeNode* root)
-//{
-//	if (root == nullptr) return 0;
-//
-//	stack<TreeNode*> ts;
-//	ts.push(root);
-//	stack<TreeNode*> tmp;
-//	int sum = 0;
-//	while (!ts.empty())
-//	{
-//		auto node = ts.top();
-//
-//		// 叶子节点
-//		if (!node->left && !node->right)
-//		{
-//			// 遍历ts
-//			int cn = 0;
-//			while (!ts.empty())
-//			{
-//				auto top = ts.top();
-//				sum += top->val* pow(10, cn++);
-//				tmp.push(top);
-//				ts.pop();
-//			}
-//
-//			while (!tmp.empty())
-//			{
-//				ts.push(tmp.top());
-//				tmp.pop();
-//			}
-//		}
-//		if (node->left)ts.push(node->left);
-//		if (node->right)ts.push(node->right);
-//		ts.pop();
-//	}
-//	return sum;
-//}
-
-
 int treeSum(TreeNode* root, int sum)
 {
-	if (root == nullptr) return 0 ;
+	if (root == nullptr) return 0;
 
 	if (root->left == nullptr && root->right == nullptr)
 	{
@@ -114,3 +74,98 @@ int sumNumbers(TreeNode* root)
 	return treeSum(root, 0);
 }
 
+
+void make_permuteUnique(vector<int>& nums, vector<vector<int>>& permutes, int p, int q)
+{
+	if (p == q)
+	{
+		return permutes.push_back(nums);
+	}
+	for (int i = p; i <= q; i++)
+	{
+		bool do_swap = true;
+		for (int j = i + 1; j <= q; j++)
+		{
+			if (nums[i] == nums[j])
+			{
+				do_swap = false;
+				break;
+			}
+		}
+		if (do_swap)
+		{
+			swap(nums[p], nums[i]);
+			make_permuteUnique(nums, permutes, p + 1, q);
+			swap(nums[p], nums[i]);
+		}
+	}
+}
+
+vector<vector<int>> permuteUnique(vector<int>& nums)
+{
+	vector<vector<int>> permutes;
+	make_permuteUnique(nums, permutes, 0, nums.size() - 1);
+	return permutes;
+}
+
+void make_permute(vector<int>& nums, vector<vector<int>>& permutes, int p, int q)
+{
+	if (p == q)
+	{
+		return permutes.push_back(nums);
+	}
+	for (int i = p; i <= q; i++)
+	{
+		swap(nums[p], nums[i]);
+		make_permute(nums, permutes, p + 1, q);
+		swap(nums[p], nums[i]);
+	}
+}
+
+vector<vector<int>> permute(vector<int>& nums)
+{
+	vector<vector<int>> permutes;
+	make_permute(nums, permutes, 0, nums.size() - 1);
+	return permutes;
+}
+
+void nextPermutation(vector<int>& nums)
+{
+	// 1.Find the largest index k such that a[k] < a[k + 1]. 
+	//   If no such index exists, the permutation is the last permutation.
+	int k = -1;
+	for (int i = 0; i < nums.size() - 1; i++)
+	{
+		if (nums[i] < nums[i + 1]) k = i;
+	}
+
+	if (k != -1)
+	{
+		// 2.Find the largest index l greater than k such that a[k] < a[l].
+		int l = k;
+		for (int i = k; i < nums.size(); i++)
+		{
+			if (nums[k] < nums[i]) l = i;
+		}
+
+		// 3.Swap the value of a[k] with that of a[l].
+		swap(nums[k], nums[l]);
+
+		// 4.Reverse the sequence from a[k + 1] up to and including the final element a[n].
+		int p = k + 1;
+		int q = nums.size() - 1;
+		while (p < q)
+		{
+			swap(nums[p++], nums[q--]);
+		}
+	}
+	else
+	{
+		int p = 0;
+		int q = nums.size() - 1;
+		while (p < q)
+		{
+			swap(nums[p++], nums[q--]);
+		}
+	}
+}
--- leetcode_everyday/main.cpp
+++ leetcode_everyday/main.cpp
@@ -2,15 +2,61 @@
 
 #include <catch.hpp>
 #include "leetcode.h"
+//
+//TEST_CASE("isCompleteTree", "[single-file]")
+//{
+//	CHECK_THAT(isCompleteTree(stringToTreeNode("[1,2,3,4,5,6]")));
+//	CHECK_THAT(!isCompleteTree(stringToTreeNode("[1,2,3,4,5,null,7]")));
+//}
+//
+//TEST_CASE("sumNumbers", "[single-file]")
+//{
+//	CHECK_THAT(sumNumbers(stringToTreeNode("[1,2,3]")) == 25);
+//	CHECK_THAT(sumNumbers(stringToTreeNode("[4,9,0,5,1]")) == 1026);
+//}
+//
+//TEST_CASE("permute", "[single-file]")
+//{
+//	auto v = vector<int>{ 1, 2, 3 };
+//	CHECK_THAT(permute(v),
+//		Catch::Matchers::Equals(
+//			vector<vector<int>>{ {1, 2, 3},
+//			{ 1, 3, 2 },
+//			{ 2, 1, 3 },
+//			{ 2, 3, 1 },
+//			{ 3, 2, 1 },
+//			{ 3, 1, 2 }}));
+//}
+//
+//TEST_CASE("permuteUnique", "[single-file]")
+//{
+//	auto v = vector<int>{ 1, 1, 2 };
+//	CHECK_THAT(permuteUnique(v),
+//		Catch::Matchers::Equals(
+//			vector<vector<int>>{ {1, 1, 2},
+//			{ 1, 2, 1 },
+//			{ 2, 1, 1 }}));
+//}
 
-TEST_CASE("isCompleteTree", "[single-file]")
+TEST_CASE("nextPermutation", "[single-file]")
 {
-	REQUIRE(isCompleteTree(stringToTreeNode("[1,2,3,4,5,6]")));
-	REQUIRE(!isCompleteTree(stringToTreeNode("[1,2,3,4,5,null,7]")));
-}
+	auto v1 = vector<int>{ 1,2,3 };
+	nextPermutation(v1);
+	CHECK_THAT(v1, Catch::Matchers::Equals(vector<int>{1, 3, 2}));
 
-TEST_CASE("sumNumbers", "[single-file]")
-{
-	REQUIRE(sumNumbers(stringToTreeNode("[1,2,3]")) == 25);
-	REQUIRE(sumNumbers(stringToTreeNode("[4,9,0,5,1]")) == 1026);
+	auto v2 = vector<int>{ 3, 2, 1 };
+	nextPermutation(v2);
+	CHECK_THAT(v2, Catch::Matchers::Equals(vector<int>{1, 2, 3}));
+
+	auto v3 = vector<int>{ 1, 1, 5 };
+	nextPermutation(v3);
+	CHECK_THAT(v3, Catch::Matchers::Equals(vector<int>{1, 5, 1}));
+
+	auto v4 = vector<int>{ 1, 2, 3,4 };
+	nextPermutation(v4);
+	CHECK_THAT(v4, Catch::Matchers::Equals(vector<int>{1, 2, 4,3}));
+
+	auto v5 = vector<int>{ 1,2 };
+	nextPermutation(v5);
+	CHECK_THAT(v5, Catch::Matchers::Equals(vector<int>{2, 1}));
 }
